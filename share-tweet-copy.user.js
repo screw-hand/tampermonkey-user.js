ded393156c82040f0c46071676faf2e80e52d8d3
undefined <null@mail.com>
2023-12-13 16:45:45 +0800

    chore: share-tweet-copy v0.2

--- share-tweet-copy.user.js
+++ share-tweet-copy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         share-tweet-copy
 // @namespace    https://screw-hand.com/
-// @version      0.1
+// @version      0.2
 // @description  support twitter to copy, easy to share.
 // @author       screw-hand
 // @match        https://twitter.com/*
