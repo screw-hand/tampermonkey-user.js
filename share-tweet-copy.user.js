d2827f4c49bacecaed3d300116a633a9911f9efb
screw-hand <screwhand0@gmail.com>
2023-12-13 09:04:18 +0800

    Add files via upload

---
+++ share-tweet-copy.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         share-tweet-copy
+// @namespace    https://screw-hand.com/
+// @version      0.0.1
+// @description  support twitter to copy, easy to share.
+// @author       screw-hand
+// @match        https://twitter.com/*
+// @icon         https://abs.twimg.com/favicons/twitter-pip.3.ico
+// @grant        none
+// @homepage     https://github.com/screw-hand/tampermonkey-user.js
+// @updateURL    https://github.com/screw-hand/tampermonkey-user.js/raw/main/share-tweet-copy.user.js
+// @downloadURL  https://github.com/screw-hand/tampermonkey-user.js/raw/main/share-tweet-copy.user.js
+// @supportURL   https://github.com/screw-hand/tampermonkey-user.js/issues/new
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	const scriptName = 'share-tweet-copy';
+	console.log(scriptName);
+	// Your code here...
+})();
