02a0203d9a546356cf4ffc91437cbdcc134a111c
screw-hand <screwhand0@gmail.com>
2025-05-21 22:53:05 +0800

    feat: 新增DEV本地用户脚本，便于外部JS脚本的切换

---
+++ _utils/dev.local.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         DEV local
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Try to take over the world!
+// @author       You
+// @match        *://*/*
+
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  /**
+   *  Include external JS scripts here for easy switching.
+   */
+  // @require      path/to/local/script.js
+	console.log('DEV local');
+})();
